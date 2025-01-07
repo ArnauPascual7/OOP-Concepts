434f2853455838e9389fd85b9683da4992e39393
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-01-07 16:37:34 +0100

    Class and Instance Example DONE

---
+++ oopconcepts/oopconcepts/Pencil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace oopconcepts
+{
+    public class Pencil
+    {
+        private string hardness;
+        private string brand;
+        
+        public string GetHardness() { return this.hardness; }
+        public string GetBrand() { return this.brand; }
+        
+        public void SetHardness(string hardness) { this.hardness = hardness; }
+        public void SetBrand(string brand) { this.brand = brand; }
+        
+        public Pencil(string hardness, string brand)
+        {
+            SetHardness(hardness);
+            SetBrand(brand);
+        }
+    }
+}
